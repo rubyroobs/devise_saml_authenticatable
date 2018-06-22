32e82113ace727a6c025e1eb2c87099ea8eebae7
Ruby <ruby@ruby.sh>
2018-06-22 22:35:52 +0100

    dont use :as on routes

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -2,9 +2,9 @@ ActionDispatch::Routing::Mapper.class_eval do
   protected
   def devise_saml_authenticatable(mapping, controllers)
     resource :session, :only => [], :controller => controllers[:saml_sessions], :path => "" do
-      get :new, :path => "saml/sign_in", :as => "new"
+      get :new, :path => "saml/sign_in"
       post :create, :path=>"saml/auth"
-      match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy", :via => mapping.sign_out_via
+      match :destroy, :path => mapping.path_names[:sign_out], :via => mapping.sign_out_via
       get :metadata, :path=>"saml/metadata"
       match :idp_sign_out, :path=>"saml/idp_sign_out", via: [:get, :post]
     end
